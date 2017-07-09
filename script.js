e9721b769bdfb11f15b7ef73c8813c64185a781a
tomanistor <toma.nistor@gmail.com>
2017-07-09 03:05:05 -0700

    Completed to-do list app

--- script.js
+++ script.js
@@ -10,7 +10,17 @@ new Vue({
     taskList: []
   },
 
-  // Methods for app
+  // Computed properties in list
+  computed: {
+    areAllChecked: function() {
+      // Check if every checked property returns true and if there is at least one to-do item
+      return this.taskList.every(function(task) {
+        return task.checked;
+      }) && this.taskList.length > 0;
+    }
+  },
+
+  // Methods for list
   methods: {
     addTask: function() {
       var task = this.newTask.trim();
@@ -21,8 +31,25 @@ new Vue({
           text: task,
           checked: false
         });
-        // Reset newTask to an empty string
+        // Reset newTask to empty string
         this.newTask = "";
+      }
+    },
+    removeTask: function(task) {
+      // Delete task from array at current index using splice
+      var index = this.taskList.indexOf(task);
+      this.taskList.splice(index, 1);
+    },
+    clearList: function() {
+      // Reset taskList to empty array
+      this.taskList = [];
+    },
+    checkAll: function() {
+      // targetValue is set to the opposite of areAllChecked
+      var targetValue = this.areAllChecked ? false : true;
+      // Set the checked state of all items to the target value
+      for (var i = 0, j = this.taskList.length; i < j; i++) {
+        this.taskList[i].checked = targetValue;
       }
     }
   }
